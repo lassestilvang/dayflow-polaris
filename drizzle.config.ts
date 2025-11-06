91058d3c4ad8843d7e025a2d1ef74b2d16aed79b
Lasse Stilvang <lasse.stilvang@gmail.com>
2025-11-06 23:36:46 +0100

    chore(config): update drizzle dialect to postgresql

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./lib/db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
     connectionString:
       process.env.DATABASE_URL ??
